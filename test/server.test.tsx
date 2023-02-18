d2c1506ec27cf41711560513afe3399df768bcf6
enpitsulin <enpitsulin@gmail.com>
2023-02-18 20:38:40 +0800

    test: base ssr test

--- test/server.test.tsx
+++ test/server.test.tsx
@@ -1,1 +1,9 @@
-export {}
+import { describe, expect, it } from 'vitest'
+import { isServer, renderToString } from 'solid-js/web'
+
+describe('environment', () => {
+  it('runs on server', () => {
+    expect(typeof window).toBe('undefined')
+    expect(isServer).toBe(true)
+  })
+})
